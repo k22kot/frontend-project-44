c8845a99b44f617b1af5ac5d3136f3e9b6cb5f6e
Kaya K <kotolot22@gmail.com>
2024-02-17 03:11:28 +0300

    rewrite even game to make it easy

--- src/games/even.js
+++ src/games/even.js
@@ -3,11 +3,9 @@ import playGame from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no"';
 
-const evenOrOdd = (number) => number % 2 === 0;
-
 const getAnswer = () => {
   const question = getRandomInt(1, 100);
-  const answer = evenOrOdd(question) ? 'yes' : 'no';
+  const answer = question % 2 === 0 ? 'yes' : 'no';
   return [question, answer];
 };
 
